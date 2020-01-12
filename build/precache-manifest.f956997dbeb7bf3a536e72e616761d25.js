0a22e3c50ff5b5f30d92af5b46d9d87fa5465303
Aditya Vardhan Singh <adityavsc137@gmail.com>
2020-01-12 06:31:44 +0000

    fix1

---
+++ build/precache-manifest.f956997dbeb7bf3a536e72e616761d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acee41671fba7fd074c15e5215cb8c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b28c4e14b6fcfd48aef",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "68ee4c58c63c8c5b6740",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "5b28c4e14b6fcfd48aef",
+    "url": "/static/js/2.186e4eaa.chunk.js"
+  },
+  {
+    "revision": "68ee4c58c63c8c5b6740",
+    "url": "/static/js/main.398af933.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
